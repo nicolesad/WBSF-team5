d28cb451017e3c3cf72f24ddb57b80ca98d1fd4a
EricRod0 <67758042+EricRod0@users.noreply.github.com>
2020-11-16 19:36:42 -0700

    Enabled dependencies in index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const pug = require('pug');
+const bodyParser = require('body-parser');
+const path = require('path');
+const routes = require('./routes/routes');
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+app.use(express.static(path.join(__dirname, '/public')));
+
+const urlencodedParser = bodyParser.urlencoded({
+    extended: true
+});
